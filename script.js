ea5ba02cdd30e32eac4cb406fc12c7314d1c98f5
Pamaq <paulinakuta00@gmail.com>
2023-03-01 14:28:26 +0100

    All the colors buttons are working now

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const pinkBtn = document.querySelector(" .pink");
+const pinkBtn = document.querySelector(".pink");
+const purpleBtn = document.querySelector(".purple");
+const blueBtn = document.querySelector(".blue");
+const greenBtn = document.querySelector(".green");
 const notepadSpace = document.querySelector(".notepadSpace");
 
 const addNewCard = (color) => {
 	const newNote = document.createElement("div");
-	newNote.textContent = "your note";
+	newNote.textContent = "your text here";
 	newNote.classList.add("note");
 	notepadSpace.append(newNote);
 	newNote.classList.add(`${color}`);
 };
-// addNewCard("pink");
-// pinkBtn.addEventListener("click", addNewCard(pink));
+
 pinkBtn.addEventListener("click", function () {
 	addNewCard("pink");
+});
+purpleBtn.addEventListener("click", function () {
+	addNewCard("purple");
+});
+blueBtn.addEventListener("click", function () {
+	addNewCard("blue");
+});
+greenBtn.addEventListener("click", function () {
+	addNewCard("green");
 });
