46e113520e114b9ce7798ef1daeb3044bcedec25
Pamaq <paulinakuta00@gmail.com>
2023-03-23 10:39:01 +0100

    Changed visual appearance of the project

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ const addNewCard = (color) => {
 	newNote.classList.add("note");
 	notepadSpace.append(newNote);
 	newNote.classList.add(`${color}`);
-	newRemoveBtn.textContent = "X";
+	newRemoveBtn.textContent = "✖️";
 	newRemoveBtn.style.alignSelf = "end";
 	newRemoveBtn.classList.add("removeAllBtn");
 	newNote.append(newRemoveBtn);
-	heartBtn.textContent = "<3";
+	heartBtn.textContent = "❤️";
 	newNote.append(heartBtn);
-	// heartBtn.addEventListener("click", function () {
-	// 	let notesArr = Array.from(querySelectorAll(".note div"));
-
-	// });
+	heartBtn.addEventListener("click", function () {
+		let notesArr = Array.from(querySelectorAll(".note div"));
+		
+	});
 	newRemoveBtn.addEventListener("click", function () {
 		newRemoveBtn.parentElement.remove();
 	});
-	noteText.textContent = "Click this text to write your note";
+	noteText.textContent = "Click this text to write your note ✏️";
 	newNote.append(noteText);
 	newNote.classList.add("noteText");
 	noteText.addEventListener("click", function () {
